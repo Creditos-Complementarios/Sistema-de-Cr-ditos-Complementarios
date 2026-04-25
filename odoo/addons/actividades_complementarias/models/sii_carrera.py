a96f87211fe67450132a9178af8de9009895bbe0
Yamato-TTGL <yamato2465@outlook.com>
2026-04-24 23:58:30 -0500

    Refactor/correccion_sintaxis
    cambios de sintaxis

--- odoo/addons/actividades_complementarias/models/sii_carrera.py
+++ odoo/addons/actividades_complementarias/models/sii_carrera.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
+from odoo import models, fields
 
 
 class SiiCarrera(models.Model):
@@ -22,15 +21,9 @@ class SiiCarrera(models.Model):
         required=True
     )
 
-    @api.constrains('clave_carrera')
-    def _check_clave_carrera_unica(self):
-        for rec in self:
-            duplicado = self.search_count([
-                ('clave_carrera', '=', rec.clave_carrera),
-                ('id', '!=', rec.id),
-            ])
-            if duplicado:
-                raise ValidationError(
-                    f'La clave de carrera "{rec.clave_carrera}" ya existe. '
-                    f'ClaveCarrera debe ser única.'
-                )
+    _constraints = [
+        models.Constraint(
+            'UNIQUE(clave_carrera)',
+            'ClaveCarrera debe ser única.'
+        )
+    ]
