f8ffcc363f846b6c90c72a5836ef5d616244f3e3
EliPrg <152450597+EliPrg@users.noreply.github.com>
2026-05-13 03:37:28 -0500

    Fix indentation for post_init_hook function

--- odoo/addons/actividades_complementarias/__init__.py
+++ odoo/addons/actividades_complementarias/__init__.py
@@ -2,7 +2,9 @@
 from . import models
 from . import wizards
 
+
 def post_init_hook(env):
     actividades = env['actividad.complementaria'].search([('alumno_ids', '!=', False)])
     for a in actividades:
         a._sincronizar_inscripciones()
+        
