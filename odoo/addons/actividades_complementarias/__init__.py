011aed07fd4a3493939ae946c3e9ac04c0cb61ea
EliPrg <152450597+EliPrg@users.noreply.github.com>
2026-05-13 03:40:08 -0500

    Add post_init_hook to synchronize enrollments

--- odoo/addons/actividades_complementarias/__init__.py
+++ odoo/addons/actividades_complementarias/__init__.py
@@ -7,3 +7,4 @@ def post_init_hook(env):
     actividades = env['actividad.complementaria'].search([('alumno_ids', '!=', False)])
     for a in actividades:
         a._sincronizar_inscripciones()     
+
