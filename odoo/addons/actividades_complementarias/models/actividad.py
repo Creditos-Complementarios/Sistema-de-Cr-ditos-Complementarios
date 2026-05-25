dc2efcea91c4e073b058daea401b9f72eecf58c9
Luis Gomez <luisgomezcetz@gmail.com>
2026-05-24 19:52:20 -0500

    fix(style): añadir salto de línea al final del archivo

--- odoo/addons/actividades_complementarias/models/actividad.py
+++ odoo/addons/actividades_complementarias/models/actividad.py
@@ -2681,3 +2681,4 @@ class ActividadDepartamento(models.Model):
 
     name = fields.Char(string='Nombre', required=True)
     jefe_id = fields.Many2one('res.users', string='Jefe de Departamento')
+    
